f5411eb5fdaa6944eff5179432dfce8b146288c0
Abhyudai2008 <64310012+Abhyudai2008@users.noreply.github.com>
2020-06-15 22:14:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(800,400);
+   var rectangle1 = createSprite(391, 381, 10, 350);
+  var rectangle2 = createSprite(405, 331, 10, 250);
+  var rectangle3 = createSprite(419, 331, 10, 250);
+  var rectangle4 = createSprite(434, 331, 10, 250);
+  var rectangle5 = createSprite(534, 331, 10, 250);
+  var rectangle6 = createSprite(549, 331, 10, 250);
+  var rectangle7 = createSprite(564, 331, 10, 250);
+  var rectangle8 = createSprite(579, 331, 10, 250);
+  var rectangle9 = createSprite(490, 250, 235, 10);
+  var rectangle10 = createSprite(490, 210, 235, 10);
+  var rectangle11 = createSprite(490, 180, 10, 50);
+  var rectangle12 = createSprite(490, 150, 35, 10);
+}
+
+
+
+
+
+function draw() {
+  background(0,0,0);  
+  drawSprites();
+}
